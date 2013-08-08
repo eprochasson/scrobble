6005a88486fb2cbee2c23648f974e270f23527d9
Emmanuel Prochasson <eprochasson@gmail.com>
2013-08-08 18:22:35 +0800

    Remove RTD build

--- build/mirror_app/tests/board.js
+++
@@ -1,6 +0,0 @@
-if (typeof __coverage__ === 'undefined') { __coverage__ = {}; }
-if (!__coverage__['/home/emmanuel/Dev/scrobble/app/tests/board.js']) {
-   __coverage__['/home/emmanuel/Dev/scrobble/app/tests/board.js'] = {"path":"/home/emmanuel/Dev/scrobble/app/tests/board.js","s":{},"b":{},"f":{},"fnMap":{},"statementMap":{},"branchMap":{}};
-}
-var __cov_cxg0IBy8cJ3REDsY7fu7VA = __coverage__['/home/emmanuel/Dev/scrobble/app/tests/board.js'];
-
